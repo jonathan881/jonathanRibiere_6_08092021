4cbda77de9751a5b800737dae003864dffd959b3
Jonathan Ribiere <ribiere88@sfr.fr>
2021-09-08 14:05:29 +0200

    configuré votre environnement de développement, avec toutes les dépendances requises pour démarrer

---
+++ app.js
@@ -0,0 +1,59 @@
+//Cette variable sert a importer express
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//Cette variable sert a créer une application express
+const app = express();
+
+//Use traite touttes les requete
+app.use((req, res, next) => {
+  //Ce headers permet d'accéder à notre API depuis n'importe quelle origine ( '*' )
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  //Ce headers permet d'ajouter les headers mentionnés aux requêtes envoyées vers notre API (Origin , X-Requested-With , etc.) ;
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  //Ce headers permetd'envoyer des requêtes avec les méthodes mentionnées ( GET ,POST , etc.).
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.post("/api/stuff", (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: "Objet créé !",
+  });
+});
+
+app.use("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
+});
+
+//Sert a exportée pour d'autre fichier
+module.exports = app;
