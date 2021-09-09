909ef290b75d19a9f5edef3d5c7ce872ff5d6ccb
Jonathan Ribiere <ribiere88@sfr.fr>
2021-09-09 08:50:04 +0200

    Créer des utilisateur dans la fonction login

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use("/api/sauces", saucesRoutes);
+app.use("/api/auth", userRoutes);
 
 //Sert a exportée pour d'autre fichier
 module.exports = app;
